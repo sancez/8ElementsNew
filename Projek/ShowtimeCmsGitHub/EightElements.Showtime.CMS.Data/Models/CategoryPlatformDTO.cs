9c21d6255254dc49f2078c7149f859be286eb67d
sance1 <sance.8elements@gmail.com>
2021-11-12 13:25:41 +0700

    no message

---
+++ Projek/ShowtimeCmsGitHub/EightElements.Showtime.CMS.Data/Models/CategoryPlatformDTO.cs
@@ -0,0 +1,15 @@
+﻿using EightElements.Showtime.CMS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EightElements.Showtime.CMS.Data.Models
+{
+    public class CategoryPlatformDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ContentPlatform> Platforms { get; set; }
+    }
+}
